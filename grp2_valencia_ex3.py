e7eb7d47911ab62b6e237ad726144eaefe6bce48
arvalencia@paterostechnologicalcollege.edu.ph <arvalencia@paterostechnologicalcollege.edu.ph>
2026-03-11 19:05:02 +0800

    Add initial script with variable examples and prints

---
+++ grp2_valencia_ex3.py
@@ -0,0 +1,59 @@
+# My first simple script
+
+print("Hello, I'm Angel Hekari Valencia! I'm from BSIT 1V")
+
+# Python variables
+number = 25
+name = "Angel Hekari"
+
+print("Number:", number)
+print("Name:", name)
+
+# Group Members
+era = "Era"
+lyka = "Lyka"
+adrian = "Adrian"
+ruzzel = "Ruzzel"
+
+print("Group Members:")
+print(era, adrian, lyka, ruzzel)
+
+# Many values to multiple variables
+x = "Apple"
+y = "Banana"
+z = "Cherry"
+
+print("Values of x, y, z:", x, y, z)
+
+# One value to multiple variables
+a = b = c = "Orange"
+print("Values of a, b, c:", a, b, c)# My first simple script
+
+print("Hello, I'm Angel Hekari Valencia! I'm from BSIT 1V")
+
+# Python variables
+number = 25
+name = "Angel Hekari"
+
+print("Number:", number)
+print("Name:", name)
+
+# Group Members
+era = "Era"
+lyka = "Lyka"
+adrian = "Adrian"
+ruzzel = "Ruzzel"
+
+print("Group Members:")
+print(era, adrian, lyka, ruzzel)
+
+# Many values to multiple variables
+x = "Apple"
+y = "Banana"
+z = "Cherry"
+
+print("Values of x, y, z:", x, y, z)
+
+# One value to multiple variables
+a = b = c = "Orange"
+print("Values of a, b, c:", a, b, c)
